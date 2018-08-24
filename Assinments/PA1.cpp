7879d5b5f288716066400372b5d5f4aa52ef325a
BuildTools <unconfigured@null.spigotmc.org>
2018-08-23 21:45:36 -0500

    adding PA1

--- Assinments/PA1.cpp
+++ Assinments/PA1.cpp
@@ -1,10 +1,19 @@
-//
-// Created by cgoodart on 8/23/18.
-//
+/*
+Caleb Goodart
+CS2433
+PA1
+*/
 
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    cout << "Name: Caleb Goodart" << endl;
+    cout << "Course: CS 2433" << endl;
+    cout << "Year: 3rd" << endl;
+    cout << "Phrase: I love Computer Science" << endl;
+    cout << "Hobbies: Gaming, biking, playing with computers" << endl;
+
     return 0;
 }
