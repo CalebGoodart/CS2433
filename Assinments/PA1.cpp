2ca237a2c8c879641fc05fbac63cd9c1bcf1c409
BuildTools <unconfigured@null.spigotmc.org>
2018-08-23 21:29:38 -0500

    intital commit

---
+++ Assinments/PA1.cpp
@@ -0,0 +1,10 @@
+//
+// Created by cgoodart on 8/23/18.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/CS2433.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS2433.dir/main.cpp.o"
+  "CMakeFiles/CS2433.dir/Assinments/PA1.cpp.o"
+  "CS2433.pdb"
+  "CS2433"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS2433.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
