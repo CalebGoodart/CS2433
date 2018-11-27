5fea2250df9ec87a510f82c5082da8aac8a9db01
BuildTools <unconfigured@null.spigotmc.org>
2018-11-27 14:57:46 -0600

    updating PA12

--- Assinments/PA12.cpp
+++ Assinments/PA12.cpp
@@ -8,26 +8,31 @@ PA12
 #include <iostream>
 
 
-double fibRecur(double);
+double fibRecur(int);
+
+//array to keep track of fib nums
+double mem[51];
 
 int main(){
 
-   int arr[50];
-
     for (int i = 1; i <= 50; ++i) {
         std::cout << fibRecur(i) << std::endl;
-
-
     }
 }
 
-double fibRecur(double n){
+double fibRecur(int n){
 
-
-    if ( n == 1 || n == 2 ){
+    if ( n <= 1 ){
         return 1;
+
+        //checks if fib num is in memory, if so returns the num in mem
+    }else if (mem[n] != 0) {
+        return mem[n];
     }else{
-        return fibRecur(n - 1) + fibRecur( n - 2);
+
+        //if the fib is not in mem, it gets added and returns the num in mem
+        mem[n] = fibRecur(n - 1) + fibRecur( n - 2);
+        return mem[n];
     }
 
 }
