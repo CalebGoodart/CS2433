008ba51995da31926ff49b34400d947844552513
BuildTools <unconfigured@null.spigotmc.org>
2018-11-27 14:18:49 -0600

    updating PA12

--- Assinments/PA12.cpp
+++ Assinments/PA12.cpp
@@ -8,45 +8,26 @@ PA12
 #include <iostream>
 
 
-int fibRecur(int);
-long int fibIter(long int);
+double fibRecur(double);
 
 int main(){
 
-    std::cout << fibIter(50) << std::endl;
+   int arr[50];
+
+    for (int i = 1; i <= 50; ++i) {
+        std::cout << fibRecur(i) << std::endl;
+
+
+    }
 }
 
-int fibRecur(int n){
+double fibRecur(double n){
 
 
-    if ( n == 1){
-        return 1;
-    }else if (n == 2){
+    if ( n == 1 || n == 2 ){
         return 1;
     }else{
-
-        if (n-1 != 2 && n-2 != 1){
-            std::cout << n - 1 <<std::endl;
-        }
-
         return fibRecur(n - 1) + fibRecur( n - 2);
     }
 
-}
-
-long int fibIter(long int n){
-    long int fibs[n + 1];
-
-    fibs[1] = 1;
-    fibs[2] = 1;
-
-    for (int i = 3; i < n + 1; i++){
-        fibs[i] = ( fibs[i - 1] + fibs[i -2]);
-    }
-
-    for (int i = 1; i < n; i++){
-
-        std::cout << fibs[i] << std::endl;
-    }
-    return fibs[n];
 }
